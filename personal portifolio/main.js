d47a05bf915021b2ee930085f4f01d1e393d1463
mahmoudelsafty2002 <111205481+mahmoudelsafty2002@users.noreply.github.com>
2024-10-22 03:58:32 +0300

    Add files via upload

---
+++ personal portifolio/main.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const menuToggle = document.querySelector(".toggle");
+  const sidebar = document.querySelector(".sidebar");
+  menuToggle.classList.toggle("actve");
+  sidebar.classList.toggle("actve");
+}
